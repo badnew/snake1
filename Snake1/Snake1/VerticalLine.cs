79c4242ba284ff0dcd550ef7814c1d31b68a0b18
badnew <badnew@mail.ru>
2016-06-30 18:51:02 +0300

    Добавлен класс VerticalLine

---
+++ Snake1/Snake1/VerticalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        // конструктор
+        public VerticalLine(int yUpper, int yLower, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUpper; y <= yLower; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
