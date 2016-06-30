e98513beef8d181deecd4c692776fdf8627c0910
badnew <badnew@mail.ru>
2016-06-30 17:57:00 +0300

    Работа со стандартным классом List

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -16,6 +16,53 @@ namespace Snake1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            // создание нового списка с символами
+            List<char> charList = new List<char>();
+            charList.Add('*');
+            charList.Add('#');
+            charList.Add('+');
+
+            foreach(char j in charList)
+            {
+                Console.WriteLine(j);
+            }
+
+            // создание дополнительных точек
+            Point p3 = new Point(10, 23, '$');
+            Point p4 = new Point(34, 3, '@');
+            Point p5 = new Point(2, 18, '%');
+
+            // создание нового списка с объектами класса Point
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p3);
+            pointList.Add(p4);
+            pointList.Add(p5);
+
+            foreach(Point k in pointList)
+            {
+                Console.WriteLine(k.sym);
+            }
+
             Console.ReadLine();
         }
     }
